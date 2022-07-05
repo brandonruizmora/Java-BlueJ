538620a81de5e6f4cdb5aa92c9e5aae6d96cb966
brandonruizmora <brandon_ruizmor@outlook.es>
2022-07-04 22:25:14 -0500

    Operadores de comparacion, logicos if else

--- PrimeraClase.java
+++ PrimeraClase.java
@@ -8,8 +8,28 @@
 public class PrimeraClase
 {
     public static void main(String[] args) {
+        //Tipos de datos  y variables
+        int numeroInt = 10;
+        double numeroDecimal = 10.05;
+        //Operadores
+        double resultado = numeroInt + numeroDecimal;
+        String datoString = "Hola Mundo, mi número es: ";
+        System.out.println(datoString + resultado);
         
-        System.out.println("Hola Mundo");
+        //Se utiliza final para denotar variables constantes
+        final int valorConstante = 123;
+        
+        int edad = 23;
+        String descripcionEdad = "";
+        
+        //Operadores comparación >, <, ==, >=, <=
+        //Operadores lógicos ||, &&
+        if(edad >= 18 && edad <= 40) {
+            descripcionEdad = "Eres mayor de edad y no tan viejo";
+        } else {
+            descripcionEdad = "Eres menor de edad";
+        }
+        System.out.println(descripcionEdad);
         
     }
 }
